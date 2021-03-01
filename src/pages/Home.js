3bcc9464b1c974cdcd15bd71d1229efa11f60309
KennyShi-designer <58654759+KennyShi-designer@users.noreply.github.com>
2021-03-01 12:14:29 +0800

    文件上传核心功能实现

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 import { useStores } from '../stores';
+import Uploader from '../components/Uploader';
+import Tips from '../components/Tips';
+
+// const Home = observer(() => {
+//   const { UserStore } = useStores();
+//   const User = () => <div>Hello, {UserStore.currentUser.attributes.username}</div>
+//   return (
+//     <>
+//       <h1>
+//         {
+//           UserStore.currentUser ? <>
+//             <User />
+//           </> : <>用户未登录</>
+//         }
+//       </h1>
+
+//       <Uploader />
+//     </>
+//   );
+// })
+
+// export default Home;
+
+
 
 const Home = observer(() => {
   const { UserStore } = useStores();
   const User = () => <div>Hello, {UserStore.currentUser.attributes.username}</div>
   return (
     <>
-      <h1>
-        {
-          UserStore.currentUser ? <>
-            <User />
-          </> : <>用户未登录</>
-        }
-      </h1>
+
+        <Tips>请先登录再上传</Tips>
+
+      <Uploader />
     </>
   );
 })
--- src/stores/index.js
+++ src/stores/index.js
@@ -1,16 +1,19 @@
 import React, { createContext, useContext } from 'react';
 import AuthStore from './auth';
 import UserStore from './user';
+import ImageStore from './image';
 
 const context = createContext({
     AuthStore,
-    UserStore
+    UserStore,
+    ImageStore
 })
 
 
 window.stores = {
     AuthStore,
-    UserStore
+    UserStore,
+    ImageStore
 }
 
 
